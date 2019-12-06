58d3dc7205f0f1c2706d62e0899cd13eec4cd066
Federicojr <46435241+Federicojr@users.noreply.github.com>
2019-12-06 23:31:58 +0800

    Add files via upload

---
+++ add.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lato:900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Lato:900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Lato:900|Oswald:700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Ewert|Kaushan+Script|Lato:900|Oswald:700" rel="stylesheet">
+	<title></title>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<style type="text/css">
+	body{
+		background-image: url(grad1.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+	
+		
+	}
+		.container{
+			height: 100%;
+			width: 100%;
+			background-color: #00800000;
+			text-align: center;
+      padding-top: 150px;
+		}
+		img{
+			height: 220px;
+			width: 220px;
+			padding-left: 30px;
+			padding: 10px;
+  border-radius: 25px;
+		}
+		.int{
+			height: 30px;
+			width:30px;
+			color: blue;
+		}
+		input:checked {
+  height: 50px;
+  width: 50px;
+}
+.h1{
+	padding-top: 50px;
+}
+element.style {
+    background-color: #fff0f533;
+    border-radius: 20px;
+}
+.navbar-inverse {
+    background-color: #7b7b7b61;
+    border-color: #b1b1b0;
+}
+.navbar-inverse .navbar-nav>.active>a, .navbar-inverse .navbar-nav>.active>a:focus, .navbar-inverse .navbar-nav>.active>a:hover {
+    color: #000;
+    background-color: #ff9900;
+    border-radius: 7px;
+    width: 100px;
+    font-family: 'Lato', sans-serif;
+    font-size: 30px;
+}
+.btn-info {
+    color: #fff;
+    background-color: #5bc0de00;
+    border-color: #65656500;
+}
+.navbar-inverse .navbar-brand {
+    color: #ffffff;
+    font-family: lato;
+    font-size: 33px;
+}
+.navbar-inverse .navbar-nav>li>a {
+    color: #ffffff;
+    font-family: lato;
+    font-size: 20px;
+}
+.h2, h2 {
+    font-size: 39px;
+    color: black;
+    background-color: #ff9900;
+    border-radius: 7px;
+  font-family: 'Oswald', sans-serif;
+
+}
+.btn-default {
+    color: #000;
+    background-color: #f90;
+    border-color: #f90;
+    font-family: lato;
+}
+	
+	a {
+    color: #ffffff;
+    text-decoration: none;
+    font-family: lato;
+    font-size: 16;
+}
+.navbar-form .form-control {
+    display: inline-block;
+    width: 350px;
+    vertical-align: middle;
+}
+.h1 {
+    padding-top: 50px;
+    background-color: #ff990000;
+    width: auto;
+    color: white;0
+    font-size: 50px;
+    font-family: 'Ewert', cursive;
+}
+.h2, h2 {
+    font-size: 39px;
+    color: black;
+    background-color: #c7c2c200;
+    border-radius: 7px;
+    font-family: 'Oswald', sans-serif;
+    padding-top: 300px;
+  }
+ .out1 {
+    background-color: #00fff338;
+    padding-top: 50px;
+ }
+ a {
+    color: #000000;
+    text-decoration: none;
+    font-size: 20px;
+    font-family: lato;
+    font-size: 16;
+}
+.btn-success {
+    color: #fff;
+    background-color: #5cb85c;
+    border-color: #4cae4c;
+    font-size: 20px;
+}
+.btn-primary {
+    color: #fff;
+    background-color: #337ab7;
+    border-color: #2e6da4;
+    font-size: 17px;
+}
+.hh{
+  font-family: 'Lobster', cursive;
+  font-size: 50px;
+	</style>
+}
+
+</head>
+<body>
+	<header>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Music</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Hub</a></li>
+      <li><a href="home1.php" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-home"></span> Home</a>
+        </li>
+      <li><a href="listen.html" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-music">Music</span></a></li>
+            <li><a href="add.php" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-plus">AddMusic</span></a></li>
+          <form class="navbar-form navbar-left" action="/action_page.php">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search" name="search">
+      </div>
+      <button type="submit" class="btn btn-default"> <span class="glyphicon glyphicon-search"><a href="">Search</a></span> </button>
+    </form>
+  </div>
+      
+    </ul>
+  </div>
+</nav>
+</header>
+<div class="container">
+  <h1 class="hh">Add and Listen to Your Songs</h1>
+  <br>
+  <label>Email Address:</label>
+  <input type="text" name="" id="mail">
+  <label>Music Tittle:</label>
+  <input type="text" name="" id="mus">
+  <label>Artist:</label>
+  <input type="text" name="" id="art">
+  <label>Album:</label>
+  <input type="text" name="" id="al">
+  <label>Audio File</label>
+  <input type="text" name="" id="aud">
+  <select id="format">
+    <option>MP3</option>
+    <option>OCC</option>
+    <option>FLAC</option>
+    <option>WMA</option>
+  </select><br>
+<button   class="btn btn-primary"onclick="music.display()">OK</button>
+<h3 id="out" class="out1"></h3>
+<button  class="btn btn-success" onclick="submit()"><span class="glyphicon glyphicon-send"></span>Submmit</button>
+
+</div>
+
+<script type="text/javascript">
+  var music={
+    mail1:"",
+    music1:"",
+    artist:"",
+    album:"",
+    audio:"",
+
+    getvalues:function(){
+      this.music1=document.getElementById("mus").value;
+      this.artist=document.getElementById("art").value;
+      this.album=document.getElementById("al").value;
+      this.audio=document.getElementById("aud").value;
+      this.mail1=document.getElementById("mail").value;
+    },
+    display:function(){
+      this.getvalues()
+      var froption=document.getElementById('format').options;
+        var frindex=document.getElementById('format').selectedIndex;
+        var selected = froption[frindex].value;
+      document.getElementById('out').innerHTML= "Email Address:"+this.mail1+"Music Tittle:"+this.music1+"<br>"+"Artist:"+this.artist+"Album:"+this.album+"Audio File:"+this.audio+"."+selected
+    }
+
+     
+
+  }
+      function submit(){
+      swal("Submmited!", "Check you Gmail for Verification", "success");
+     }
+
+</script>
+
+
+
+
+
+</body>
+</html>
